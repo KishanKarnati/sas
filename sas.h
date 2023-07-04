1b2bc2873951c4b8d51eba0163cccbe77799fc59
KishanKarnati <103353754+KishanKarnati@users.noreply.github.com>
2023-07-04 16:54:12 +0530

    Update sas.h

--- sas.h
+++ sas.h
@@ -1,32 +1,35 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-
+// Definition of constants for maximum values
 #define MAX_STUDENTS 50
 #define MAX_NAME_LENGTH 50
-#define MAX_SURNAME_LENGTH 50
-#define MAX_FATHERS_NAME_LENGTH 50
-#define MAX_MOTHERS_NAME_LENGTH 50
 #define MAX_EMAIL_LENGTH 50
-#define MAX_PHONE_LENGTH 15
-#define MAX_ADDRESS_LENGTH 100
+#define MAX_PHONE_LENGTH 10
 #define MAX_COURSE_LENGTH 50
 
+// Definition of the structure to represent a student
 typedef struct {
-    int id;                                 // ID of the student
-    char name[MAX_NAME_LENGTH];             // Name of the student
-    char surname[MAX_SURNAME_LENGTH];       // Surname of the student
-    char fathersName[MAX_FATHERS_NAME_LENGTH];   // Father's name of the student
-    char mothersName[MAX_MOTHERS_NAME_LENGTH];   // Mother's name of the student
-    char email[MAX_EMAIL_LENGTH];           // Email of the student
-    char phone[MAX_PHONE_LENGTH];           // Phone number of the student
-    char address[MAX_ADDRESS_LENGTH];       // Address of the student
-    char course[MAX_COURSE_LENGTH];         // Course of the student
+    int id;                                 // Student ID
+    char name[MAX_NAME_LENGTH];             // Student name
+    char email[MAX_EMAIL_LENGTH];           // Student email
+    char phone[MAX_PHONE_LENGTH];           // Student phone number
+    char course[MAX_COURSE_LENGTH];         // Student course
 } Student;
 
-void addStudent(Student* students[], int* count);
-void displayStudents(Student* students[], int count);
-void searchStudent(Student* students[], int count);
+// Function prototypes for validating student information
+int validateName(const char* name);                     // Validates the student name
+int validateEmail(const char* email);                   // Validates the student email
+int validatePhoneNumber(const char* phoneNumber);       // Validates the student phone number
+int validateCourse(const char* course);                 // Validates the student course
+
+// Function prototypes for managing student records
+void addStudent(Student* students[], int* count);        // Adds a new student record
+void displayStudents(Student* students[], int count);    // Displays all student records
+void searchStudent(Student* students[], int count, int searchId);  // Searches for a student by ID
+void deleteStudent(Student* students[], int* count, int deleteId);  // Deletes a student record by ID
+
+// Function for freeing memory allocated for student records
 void freeMemory(Student* students[], int count);
 
 #endif
