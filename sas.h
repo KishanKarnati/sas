d65588670c6bbabcc6c29a5e54b83b6602c5a178
KishanKarnati <103353754+KishanKarnati@users.noreply.github.com>
2023-07-04 10:32:20 +0530

    Update sas.h

--- sas.h
+++ sas.h
@@ -1,6 +1,7 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+
 #define MAX_STUDENTS 50
 #define MAX_NAME_LENGTH 50
 #define MAX_SURNAME_LENGTH 50
@@ -12,15 +13,15 @@
 #define MAX_COURSE_LENGTH 50
 
 typedef struct {
-    int id;
-    char name[MAX_NAME_LENGTH];
-    char surname[MAX_SURNAME_LENGTH];
-    char fathersName[MAX_FATHERS_NAME_LENGTH];
-    char mothersName[MAX_MOTHERS_NAME_LENGTH];
-    char email[MAX_EMAIL_LENGTH];
-    char phone[MAX_PHONE_LENGTH];
-    char address[MAX_ADDRESS_LENGTH];
-    char course[MAX_COURSE_LENGTH];
+    int id;                                 // ID of the student
+    char name[MAX_NAME_LENGTH];             // Name of the student
+    char surname[MAX_SURNAME_LENGTH];       // Surname of the student
+    char fathersName[MAX_FATHERS_NAME_LENGTH];   // Father's name of the student
+    char mothersName[MAX_MOTHERS_NAME_LENGTH];   // Mother's name of the student
+    char email[MAX_EMAIL_LENGTH];           // Email of the student
+    char phone[MAX_PHONE_LENGTH];           // Phone number of the student
+    char address[MAX_ADDRESS_LENGTH];       // Address of the student
+    char course[MAX_COURSE_LENGTH];         // Course of the student
 } Student;
 
 void addStudent(Student* students[], int* count);
